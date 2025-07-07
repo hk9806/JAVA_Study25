84cec65fdd0a15eac8d24e3c0458dacc61d39cf4
220-02\kh0617 <ghdrudgnss9806@gmail.com>
2025-07-07 17:34:03 +0900

    2025-07-07

--- MBCBoard/src/mbcboard/sql/system.sql
+++
@@ -1,5 +0,0 @@
-create user boardtest identified by boardtest	;
-grant resource, connect to boardtest	;
-alter user boardtest default tablespace users	;
-alter user boardtest temporary tablespace temp		;
-
